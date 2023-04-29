fb7fc293a5f403c726f56912716258224f557ea9
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-29 20:32:05 +0900

    Update config.js

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -1,58 +1,8 @@
-const { description } = require('../../package')
-
-module.exports = {
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#title
-   */
+export default {
   title: 'LINE-internal-URL project',
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#description
-   */
-  description: description,
+  description: 'LINE内部スキーマをまとめるプロジェクト。',
 
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
-  head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
-  ],
-
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
   themeConfig: {
-    repo: '',
-    editLinks: false,
-    docsDir: '',
-    editLinkText: '',
-    lastUpdated: false,
-    nav: [
-      {
-        text: 'Introduction',
-        link: '/introduction/',
-      },
-      {
-        text: 'GitHub',
-        link: 'https://github.com/nakasyou/LINE-internal-URL',
-      },
-    ],
-    sidebar: {
-
-    }
-  },
-
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ],
-  base: "/LINE-internal-URL",
+    // theme-level options
+  }
 }
