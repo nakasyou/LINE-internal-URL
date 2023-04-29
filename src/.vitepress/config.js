82ffa5c509ab57711c8f3ac29cc82059ec8d7e0a
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-29 20:35:31 +0900

    Update config.js

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -1,8 +1,21 @@
-export default {
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
   title: 'LINE-internal-URL project',
   description: 'LINE内部スキーマをまとめるプロジェクト。',
-
+  head: [
+    ['meta', { name: 'theme-color', content: '#3c8772' }],
+    [
+      'script',
+      {
+        src: 'https://cdn.usefathom.com/script.js',
+        'data-site': 'AZBRSFGG',
+        'data-spa': 'auto',
+        defer: ''
+      }
+    ]
+  ],
   themeConfig: {
     // theme-level options
   }
-}
+})
